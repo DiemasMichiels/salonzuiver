45295326cc626ba997ff56fb0f2a77b675870300
Diemas <info@diemas.dev>
2022-02-20 02:56:58 +0100

    feat: terms and conditions

--- slices/Terms/index.tsx
+++ slices/Terms/index.tsx
@@ -1,4 +1,5 @@
 import RichText from '@components/prismic/RichText'
+import * as styled from './styled'
 import type { Slice, RichTextField } from '@prismicio/types'
 import type { PRISMIC_SLICES } from '@utils/prismic/constants'
 
@@ -18,9 +19,15 @@ type Props = {
 }
 
 const Terms = ({ slice }: Props) => (
-  <section>
+  <styled.Section id='cta'>
     <RichText render={slice.primary.title} />
-  </section>
+    {slice.items.map((item, i) => (
+      <div key={i}>
+        <RichText render={item.subtitle} />
+        <RichText render={item.description} />
+      </div>
+    ))}
+  </styled.Section>
 )
 
 export default Terms
---
+++ slices/Terms/styled.ts
@@ -0,0 +1,20 @@
+import styled from 'styled-components'
+import { ContainerStyle } from '@components/general/styled'
+
+export const Section = styled.section`
+  ${ContainerStyle}
+
+  h2 {
+    padding: 40px 0 40px 0;
+  }
+
+  margin-bottom: 52px;
+
+  div {
+    margin-bottom: 40px;
+
+    p {
+      padding-top: 16px;
+    }
+  }
+`
